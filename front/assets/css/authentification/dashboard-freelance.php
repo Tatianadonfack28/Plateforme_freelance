99cabc00506bb140ba40307b55e06226466a735a
Tatianadonfack28 <tatianadonfack28@gmail.com>
2026-07-29 23:13:23 +0100

    Add files via upload

---
+++ front/assets/css/authentification/dashboard-freelance.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Tableau de bord-Freelance</title>
+        <link rel="stylesheet" href="../assets/css/style.css">
+
+    </head>
+    <body>
+        <h1>Bienvenue</h1>
+
+        <nav>
+            <a href="../offres/liste-offres.html">Voir les offres disponibles</a>
+            <a href="../candidatures/mes-candidatures.html">Mes candidatures</a>
+            <a href="../authentification/deconnexion.php">Se deconnecter</a> 
+
+        </nav>
+    </body>
+</html>
